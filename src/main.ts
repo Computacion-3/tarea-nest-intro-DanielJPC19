f99c0bc835e05267af26d3aac8a1386cb1d4f487
kelocoes <kevin.rodriguez109@gmail.com>
2025-09-13 16:47:27 -0500

    Nest start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// Or import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
